4f0f5dd1e28f110af6b48be1d434bf833c584e97
Abdalla Kordeir <115191289+Abdalla-Kordeir1@users.noreply.github.com>
2022-11-10 22:35:36 +0000

    Finished project 1

--- Loops/Exercise_3.py
+++ Loops/Exercise_3.py
@@ -2,5 +2,5 @@
 # Create a loop that runs 3 times
 
 # FIXME
-for i in 3:
+
   
--- Project1/main.py
+++ Project1/main.py
@@ -1,32 +1,33 @@
 # Copyright 2022 PI515
 
 import random
-
 keepAsking = True
 while keepAsking:
     num = input('Type a number for an upper bound: ')
-    if num.isdigit():
+    if num.isdigit() == True:
         print("Let's play!")
         num = int(num)
+        keepAsking = False
+        print(num)
         # something about the loop condition
     else:
         print("Invalid input. Try again.")
 
 secret = random.randint(1, num)
 guess = None
-count = 1
+count = 0
 
 while guess != secret:
     guess = input("Type a number between 1 and " + str(num) + ": ")
     if guess.isdigit():
         guess = int(guess)
-    if False: # can't always be wrong
-        print("You got it!")
-    else:
-        print("Try again.")
+        if guess == secret: # can't always be wrong
+      
+         print("You got it! it took you", count, "guesses")
+          
+        else:
+            print("Try again.")
+            count += 1
         # what else?
 
-if count == 1:
-    print("It took you", count, "guess!")
-else:
-    print("It took you", count, "guesses!")
+
